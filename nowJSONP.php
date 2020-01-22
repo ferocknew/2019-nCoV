35950713faa924d70a031c4d000250d5756cd2b9
Joanh.Fu <ferock@gmail.com>
2020-01-22 17:38:41 +0800

    update code

--- nowJSONP.php
+++ nowJSONP.php
@@ -1,4 +1,5 @@
 <?php
 header("Content-Type: application/json");
+header("access-control-allow-origin: *");
 $res = file_get_contents("./now.json");
 echo $_GET['callback'] . "(" . json_encode($res) . ")";
